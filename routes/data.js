e5a8d8ee18f3f41fda2b388bae75a9ff593ed547
VictorIJnr <victorijnr@gmail.com>
2018-12-18 21:57:57 +0000

    Well it's been a long time since I worked on this
    I actually don't know if it's needed now... Oh well. It's here though.
    But yeah all of the changes here are from a month and a half ago I
    think, so don't look at me for an explanation. This is all on you.

--- routes/data.js
+++ routes/data.js
@@ -2,8 +2,118 @@ let express = require("express");
 let router = express.Router();
 let path = require("path");
 let fs = require("fs");
+let request = require("request");
+let AWS = require('aws-sdk');
 
-let rootPath = path.join(__dirname, "..");
+let myDigiRegion = "nyc3";
+let myDigiSpace = "distbu";
+let myDigiOceanKey = "WVXMXAZGXWSAPE6PNBTU";
+let myDigiSecret = "4HhQJtzuam1k9DWF9RY4h8r+BMCLZ29ug1Ki8IQZUCc";
+let myDigiPoint = new AWS.Endpoint(`${myDigiRegion}.digitaloceanspaces.com/`);
+
+let s3 = new AWS.S3({
+    endpoint: myDigiPoint,
+    accessKeyId: myDigiOceanKey,
+    secretAccessKey: myDigiSecret
+});
+
+router.get("/", function(req, res) {
+    let digiParams = {
+        Bucket: myDigiSpace
+    }
+
+    s3.listObjectsV2(digiParams, (err, data) => {
+        if (err) res.send(err);
+        else {
+            files = [];
+            data.Contents.forEach(file => {
+                //Omitting folders (also any empty file)
+                if (file.Size != 0) {
+                    let fileName = file.Key.substring(file.Key.lastIndexOf("/") + 1);
+                    console.log(fileName);
+                } 
+            });
+
+            res.send(data);
+        }
+    });
+});
+
+router.get("/:dataset/:type", function(req, res) {
+    let digiParams = {
+        Bucket: myDigiSpace,
+        Key: `data/${req.params.dataset}/${req.params.type}`,
+        ResponseContentType: "application/json"
+    }
+
+    s3.getObject(digiParams, (err, data) => {
+        if (err) res.send(err);
+        else {
+            console.log(JSON.parse(data.Body));
+            res.send(data)};
+    });
+
+   /*  getFile(req.params.dataset, req.params.type)
+    .then((data) => {
+        console.log("la la")
+        res.send(data)
+    })
+    .catch((err) => res.send(err)); */
+});
+
+function getFile(dataset, file) {
+    let digiParams = {
+        Bucket: myDigiSpace
+    }
+    
+    let slippy = new Promise((resolve, reject) => {
+        getFileType(dataset, file)
+        .then((extension) => {
+            console.log("choo choo");
+            digiParams.Key = `data/${dataset}/${file}.${extension}`;
+            s3.getObject(digiParams, (err, data) => {
+                if (err) reject(err);
+                else resolve(data);
+            });
+        })
+        .catch((err) => reject(err));
+    }); 
+
+    return slippy;
+}
+
+function getFileType(dataset, fileName) {
+    let digiParams = {
+        Bucket: myDigiSpace,
+        StartAfter: `data/${dataset}`
+    };
+
+    let typePromise = new Promise((resolve, reject) => {
+        s3.listObjectsV2(digiParams, (err, data) => {
+            if (err) res.send(err);
+            else {
+                let extension;
+                data.Contents.forEach(file => {
+                    //Omitting folders (also any empty file)
+                    if (file.Size != 0) {
+                        let name = file.Key.substring(file.Key.lastIndexOf("/") + 1);
+
+                        console.log(name.substring(0, name.lastIndexOf(".")));
+
+                        if (name.substring(0, name.lastIndexOf(".")) === fileName)
+                            extension = name.substring(name.lastIndexOf(".") + 1);
+                    } 
+                });
+
+                if (extension) resolve(extension);
+                else reject("File not found");
+            }
+        });
+    });
+
+    
+    return typePromise;
+}
 
 function sendError(message) {
     let error = {
@@ -13,26 +123,5 @@ function sendError(message) {
 
     return error;
 }
-
-router.get("/:dataset/:type", function(req, res, next) {
-    let dirFiles;
-    dataPath = path.join(rootPath, "data/", req.params.dataset);
-
-    /*
-        DON'T QUERY MARCO WITHOUT GRAPHQL PAGINATION
-        HOLY FUCK IT TAKES FOREVER TO LOAD, PLUS IT HEATS
-        UP THIS BOI QUITE A FREAKING BIT
-    */
-    if (!fs.existsSync(dataPath)) {
-        res.json(sendError("File not found"));
-        return;
-    }
-
-    dirFiles = fs.readdirSync(dataPath);
-    dirFiles.forEach(file => {
-        if (file.includes(req.params.type))
-            res.sendFile(path.join(dataPath, file));
-    });
-});
 
 module.exports = router;
