a88c743ab731da36930a86d8411b62b7b5a9813e
VictorIJnr <victorijnr@gmail.com>
2019-01-03 19:30:29 +0000

    There's a new commit in here

--- routes/data.js
+++ routes/data.js
@@ -17,6 +17,11 @@ let s3 = new AWS.S3({
     secretAccessKey: myDigiSecret
 });
 
+/**
+* Index to yeet back a list of files stored on DigitalOcean
+* It's actually a little bit more than that because I don't do any pre-processing
+* but it's fine. Trust me. You know what, I'm getting rid of that useless loop.
+*/
 router.get("/", function(req, res) {
     let digiParams = {
         Bucket: myDigiSpace
@@ -24,18 +29,7 @@ router.get("/", function(req, res) {
 
     s3.listObjectsV2(digiParams, (err, data) => {
         if (err) res.send(err);
-        else {
-            files = [];
-            data.Contents.forEach(file => {
-                //Omitting folders (also any empty file)
-                if (file.Size != 0) {
-                    let fileName = file.Key.substring(file.Key.lastIndexOf("/") + 1);
-                    console.log(fileName);
-                } 
-            });
-
-            res.send(data);
-        }
+        else res.send(data);
     });
 });
 
@@ -46,29 +40,38 @@ router.get("/:dataset/:type", function(req, res) {
         ResponseContentType: "application/json"
     }
 
-    s3.getObject(digiParams, (err, data) => {
-        if (err) res.send(err);
-        else {
-            console.log(JSON.parse(data.Body));
-            res.send(data)};
-    });
-
-   /*  getFile(req.params.dataset, req.params.type)
+    getFile(req.params.dataset, req.params.type)
     .then((data) => {
-        console.log("la la")
-        res.send(data)
+        /**
+        * I don't know what was wrong with me the last time I was working on this.
+        * First "choo choo", now "la la". Maybe I was having a breakdown or something.
+        * Idk but they're staying here forever now. Why? Because I said so. 
+        */
+        console.log("la la");
+        console.log(data);
+        console.log("transed");
+        console.log(data.Body);
+        res.send(JSON.parse(data.Body));
     })
-    .catch((err) => res.send(err)); */
+    .catch((err) => res.send(err));
 });
 
 function getFile(dataset, file) {
     let digiParams = {
         Bucket: myDigiSpace
     }
+
+    console.log(`Requesting:\t${dataset}/${file}`);
     
     let slippy = new Promise((resolve, reject) => {
         getFileType(dataset, file)
         .then((extension) => {
+            /**
+            * I stopped working on this for a month and a half. 
+            * I came back, ran distbu, requested train.json (standard)
+            * then I saw "choo choo" in the console. NGL I thought it was 
+            * pretty funny. 
+            */
             console.log("choo choo");
             digiParams.Key = `data/${dataset}/${file}.${extension}`;
             s3.getObject(digiParams, (err, data) => {
@@ -82,6 +85,11 @@ function getFile(dataset, file) {
     return slippy;
 }
 
+/**
+* Gets the extension for the requested file.
+* @param {String} dataset the dataset to pull the file from 
+* @param {String} fileName the file to be retrieved from that dataset
+*/
 function getFileType(dataset, fileName) {
     let digiParams = {
         Bucket: myDigiSpace,
@@ -97,14 +105,12 @@ function getFileType(dataset, fileName) {
                     //Omitting folders (also any empty file)
                     if (file.Size != 0) {
                         let name = file.Key.substring(file.Key.lastIndexOf("/") + 1);
-
-                        console.log(name.substring(0, name.lastIndexOf(".")));
-
                         if (name.substring(0, name.lastIndexOf(".")) === fileName)
                             extension = name.substring(name.lastIndexOf(".") + 1);
                     } 
                 });
 
+                console.log(`File Extension:\t${extension}`);
                 if (extension) resolve(extension);
                 else reject("File not found");
             }
