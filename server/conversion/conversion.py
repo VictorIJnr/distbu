f472e88c59d2d632e0895ee6c5030353697260bf
VictorIJnr <victorijnr@gmail.com>
2019-04-27 03:16:30 +0100

    Began work on conversion
    So this is the CSV to JSON conversion. See what I can do if you give me
    one minute? So the Python can connect to Digi, all I need to do is make
    a loop to download each CSV file, convert it into a CSV, and upload it
    back to Digi. Efficiency? Never heard of it.

--- server/conversion/conversion.py
+++ server/conversion/conversion.py
@@ -2,32 +2,55 @@
 # Ideally, this would instead pull all the CSVs stored in a DigitalOcean folder
 # and convert those into JSONs and then dump the JSONs back into DigitalOcean.
 
+#pylint: disable=import-error
+import json
+import os
+
 import boto3
+
+from pprint import pprint
+
 from botocore.client import Config
 
-# Making a Spaces session.
-mySession = boto3.session.Session()
-
-# Client needs t
-client = mySession.client('s3',
-                        region_name='nyc3',
-                        endpoint_url='https://nyc3.digitaloceanspaces.com',
-                        aws_access_key_id='532SZONTQ6ALKBCU94OU',
-                        aws_secret_access_key='zCkY83KVDXD8u83RouEYPKEm/dhPSPB45XsfnWj8fxQ')
-
-# Create a new Space.
-client.create_bucket(Bucket='my-new-space')
-
-# List all buckets on your account.
-response = client.list_buckets()
-spaces = [space['Name'] for space in response['Buckets']]
-print("Spaces List: %s" % spaces)
+serverPath = os.path.dirname(os.path.realpath(__file__))
 
 """
 Runs the conversion of CSVs stored on DigitalOcean into JSONs.
 """
 def conversion():
-    pass
+    digiConfig = getConfig()
+    digiClient = digiConnect()
+
+    pprint(digiClient.list_objects_v2(Bucket=digiConfig["digiSpace"])["Contents"])
+
+"""
+Loads the DigitalOcean config file into memory so I can connect to DigiOcean. 
+Well, to my Space in particular
+"""
+def getConfig():
+    with open(os.path.join("..", "..", "config", "docean.json"), "r") as configJSON:
+        return json.load(configJSON)
+
+"""
+Connects to DigitalOcean and returns the resulting client suitable for requests.
+"""
+def digiConnect():
+    digiConfig = getConfig()
+    endpoint = f"https://{digiConfig['digiRegion']}.digitaloceanspaces.com/"
+    
+    mySession = boto3.session.Session()
+    digiClient = mySession.client("s3", 
+                    region_name=digiConfig["digiRegion"],
+                    endpoint_url=endpoint,
+                    aws_access_key_id=digiConfig["digiKey"],
+                    aws_secret_access_key=digiConfig["digiSecret"])
+
+    # List all buckets on your account.
+    # response = client.list_buckets()
+    # spaces = [space['Name'] for space in response['Buckets']]
+    # print("Spaces List: %s" % spaces)
+
+    return digiClient
 
 if __name__ == "__main__":
     conversion()
