2119bc9ea124b541d08d5dc64609a54da5570eca
VictorIJnr <victorijnr@gmail.com>
2019-04-28 02:01:35 +0100

    Completed CSV to JSON conversion.
    Now let's use it to test pagination and stuff I guess. I probably will
    need to keep the JSON representation of the data regardless. I can't
    paginate through CSVs without processing them into a JSONesque structure
    in the first place.

--- server/conversion/conversion.py
+++ server/conversion/conversion.py
@@ -23,28 +23,31 @@ def conversion():
     
     #? Getting the names of all the files; don't need directories 
     allObjs = digiClient.list_objects_v2(Bucket=digiConfig["digiSpace"])["Contents"]
-    allObjs = [myObj for myObj in allObjs if myObj["Size"] != 0 and not myObj["Key"].endswith("/")]
+    allObjs = [myObj for myObj in allObjs if myObj["Size"] != 0 and myObj["Key"].endswith(".csv")]
 
-    #// objList = {myObj["Key"]: myObj["Size"] for myObj in allObjs}
-    objList = [myObj["Key"] for myObj in allObjs]
+    objList = [myObj["Key"] for myObj in allObjs if "Users.csv" in myObj["Key"]]
 
-    pprint(objList)
+    for myKey in objList:
+        print(f"Converting {myKey} to a JSON file")
 
-    myFile = digiClient.get_object(Bucket=digiConfig["digiSpace"],
-                            Key=objList[-1],
-                            ResponseContentType="application/json")["Body"]
-    fileData = myFile.read()
-    decoded = fileData.decode("utf-8")
-    # pprint(type(json.loads(fileData)[0]))
-    pprint(decoded)
-    # pprint(type(decoded))
+        myFile = digiClient.get_object(Bucket=digiConfig["digiSpace"],
+                                Key=myKey,
+                                ResponseContentType="text/csv")["Body"]
+        fileData = myFile.read()
+        decoded = fileData.decode("utf-8")
 
-    #! It works since the data is a bunch of numbers
+        splitData = decoded.split("\n")
+        csvHeaders = splitData[0].split(",")
+        splitData = splitData[1:]
 
-    # for myKey in objList:
-    #     myFile = digiClient.get_object(Bucket=digiConfig["digiSpace"],
-    #                         Key=myKey)["Body"]
-    #     pprint(myFile.read())
+        dictList = [{key: value for key, value in zip(csvHeaders, myRecord.split(","))} 
+                        for myRecord in splitData]
+
+        digiClient.put_object(Bucket=digiConfig["digiSpace"],
+                                Body=json.dumps(dictList),
+                                ContentType="application/json",
+                                Key=genFileName(myKey),
+                                ACL="public-read")
 
 """
 Loads the DigitalOcean config file into memory so I can connect to DigiOcean. 
@@ -74,6 +77,13 @@ def digiConnect():
     # print("Spaces List: %s" % spaces)
 
     return digiClient
+
+"""
+The files I upload back up to Digi aren't CSVs anymore. So don't call them CSVs.
+"""
+def genFileName(fileName):
+    extensionless = fileName.split(".")[:-1]
+    return "".join(extensionless) + ".json"
 
 if __name__ == "__main__":
     conversion()
